70231d8aba8aa51c802bd77a37f55ce972899b29
jbprojectlab <james.byrd1265@gmail.com>
2019-06-27 00:16:38 -0400

    responsive: rows

--- src/app.js
+++ src/app.js
@@ -2,31 +2,49 @@ import React, {useState, useEffect, Fragment} from 'react'
 import Sampler from './sampler'
 import './css/app.css'
 
-const keyVals = ['r', 't', 'y', 'u', 'f', 'g', 'h', 'j']
-
 const App = () => {
+  const rows = [['r', 't'], ['y', 'u'], ['f', 'g'], ['h', 'j']]
   const [pressedKey, setPressedKey] = useState('')
 
   const startSamplers = context => {
     if(context.state === 'suspended') context.resume()
   }
 
-  const row1 = keyVals.slice(0, 4)
-  const row2 = keyVals.slice(4)
+
   return (
     <div>
       <h2 id='title-header'>sample-X</h2>
       <div className='row'>
-        {row1.map(keyVal => <Sampler
-          keyVal={keyVal}
-          start={startSamplers}
-        />)}
+        <div className='section'>
+          <div className='container'>
+            {rows[0].map(keyVal => <Sampler
+              keyVal={keyVal}
+              start={startSamplers}
+            />)}
+          </div>
+          <div className='container'>
+            {rows[1].map(keyVal => <Sampler
+              keyVal={keyVal}
+              start={startSamplers}
+            />)}
+          </div>
+        </div>
       </div>
-      <div className='row'>
-        {row2.map(keyVal => <Sampler
-          keyVal={keyVal}
-          start={startSamplers}
-        />)}
+      <div className='bottom row'>
+        <div className='section'>
+          <div className='container'>
+            {rows[2].map(keyVal => <Sampler
+              keyVal={keyVal}
+              start={startSamplers}
+            />)}
+          </div>
+          <div className='container'>
+            {rows[3].map(keyVal => <Sampler
+              keyVal={keyVal}
+              start={startSamplers}
+            />)}
+          </div>
+        </div>
       </div>
     </div>
   )
